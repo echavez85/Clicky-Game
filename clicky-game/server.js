baea94523cd302763c650aa2963b041d3fe83508
echavez85 <echavez85@gmail.com>
2019-03-09 16:13:43 -0600

    Added files to components

---
+++ clicky-game/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// define middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+// api routes
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening o port ${PORT}!`);
+});
